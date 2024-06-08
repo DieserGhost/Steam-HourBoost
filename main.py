9749062d673d65f26e22c76149852e6a0adfe33b
DieserGhost <red99baron@t-online.de>
2024-06-08 18:06:24 +0200

    Added to GitHub

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import sys
+
+pygame.init()
+
+# Fenstergro?e festlegen
+window_size = (400, 200)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Hour Boost")
+
+# Farben definieren
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+font = pygame.font.SysFont(None, 55)
+
+text = font.render("Now Boosting!", True, white)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(black)
+
+    text_rect = text.get_rect(center=(window_size[0] // 2, window_size[1] // 2))
+    screen.blit(text, text_rect)
+
+    pygame.display.flip()
+
+# Pygame beenden
+pygame.quit()
+sys.exit()
